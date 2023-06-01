6a3209972b33a6f511514cca3c5601e1a0728f69
LUIZ FELIPE PABLOS BUGALHO <felipebugalho2016@gmail.com>
2023-06-01 10:52:52 -0300

    Advertising (publicidade) - FULL (COMPLETO)

--- publicidade/Form1.Designer.cs
+++ publicidade/Form1.Designer.cs
@@ -28,13 +28,248 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.txtDuracao = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCadastarPubli = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtNome.Location = new System.Drawing.Point(19, 37);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(183, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtDescricao.Location = new System.Drawing.Point(19, 101);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(183, 20);
+            this.txtDescricao.TabIndex = 1;
+            // 
+            // txtDuracao
+            // 
+            this.txtDuracao.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtDuracao.Location = new System.Drawing.Point(224, 37);
+            this.txtDuracao.Name = "txtDuracao";
+            this.txtDuracao.Size = new System.Drawing.Size(169, 20);
+            this.txtDuracao.TabIndex = 2;
+            // 
+            // txtValor
+            // 
+            this.txtValor.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtValor.Location = new System.Drawing.Point(224, 101);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(75, 20);
+            this.txtValor.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mulish ExtraLight", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(16, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mulish ExtraLight", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(221, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Duração:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Mulish ExtraLight", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(16, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Descrição:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Mulish ExtraLight", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(221, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Valor:";
+            // 
+            // btnCadastarPubli
+            // 
+            this.btnCadastarPubli.Font = new System.Drawing.Font("Mulish ExtraLight", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastarPubli.Location = new System.Drawing.Point(224, 156);
+            this.btnCadastarPubli.Name = "btnCadastarPubli";
+            this.btnCadastarPubli.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastarPubli.TabIndex = 8;
+            this.btnCadastarPubli.Text = "Cadastrar";
+            this.btnCadastarPubli.UseVisualStyleBackColor = true;
+            this.btnCadastarPubli.Click += new System.EventHandler(this.btnCadastarPubli_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.txtID);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtNome);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btnCadastarPubli);
+            this.panel1.Controls.Add(this.txtDescricao);
+            this.panel1.Controls.Add(this.btnBuscar);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtValor);
+            this.panel1.Controls.Add(this.btnDeletar);
+            this.panel1.Controls.Add(this.btnAtualizar);
+            this.panel1.Controls.Add(this.txtDuracao);
+            this.panel1.Location = new System.Drawing.Point(2, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(419, 194);
+            this.panel1.TabIndex = 9;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Mulish ExtraLight", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(315, 76);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Id:";
+            // 
+            // txtID
+            // 
+            this.txtID.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtID.Location = new System.Drawing.Point(316, 101);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(77, 20);
+            this.txtID.TabIndex = 9;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Mulish ExtraLight", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(316, 156);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Font = new System.Drawing.Font("Mulish ExtraLight", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeletar.Location = new System.Drawing.Point(19, 156);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(75, 23);
+            this.btnDeletar.TabIndex = 1;
+            this.btnDeletar.Text = "Deletar";
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Font = new System.Drawing.Font("Mulish ExtraLight", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtualizar.Location = new System.Drawing.Point(127, 156);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnAtualizar.TabIndex = 0;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightGray;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(419, 48);
+            this.panel2.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Mulish ExtraLight ExtraBold", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Cadastrar Serviço";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(424, 238);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.TextBox txtDuracao;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCadastarPubli;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtID;
     }
 }
 
--- publicidade/Form1.cs
+++ publicidade/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,162 @@ namespace publicidade
         public Form1()
         {
             InitializeComponent();
+        }
+
+        
+        int idServico;
+
+        private void btnCadastarPubli_Click(object sender, EventArgs e)
+        {
+            //criando o objeto de conexão com banco de dados
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "Server=turmassenacsantos.mssql.somee.com;Database=turmassenacsantos;User Id=senacclovis_SQLLogin_1;Password=n1642mlxmm;";
+
+            //criando o objeto de comando de SQL para enviar instruções para o banco de dados
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection; //ligando o comando a conexão que configurada acima
+            cmd.CommandText = "piServico";
+            cmd.CommandType = CommandType.StoredProcedure; //definindo que o comando é um procedimento
+
+            //vincular os campos do formulários aos parâmetros do procedimento
+            cmd.Parameters.AddWithValue("nome", txtNome.Text);
+            cmd.Parameters.AddWithValue("descricao", txtDescricao.Text);
+            cmd.Parameters.AddWithValue("duracao", txtDuracao.Text);
+            cmd.Parameters.AddWithValue("valor", txtValor.Text);
+
+
+            connection.Open();
+            cmd.ExecuteNonQuery(); //executar para comandos que não possuem retorno de dados (INSERT, UPDATE e DELETE)
+            connection.Close();
+
+            MessageBox.Show("Serviço cadastrado com sucesso!");
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Deseja realmente atualizar?", "Aviso!", MessageBoxButtons.YesNo);
+
+            if (dialog == DialogResult.Yes)
+            {
+                //criando o objeto de conexão com banco de dados
+                SqlConnection connection = new SqlConnection();
+                connection.ConnectionString = "Server=turmassenacsantos.mssql.somee.com;Database=turmassenacsantos;User Id=senacclovis_SQLLogin_1;Password=n1642mlxmm;";
+
+                //criando o objeto de comando de SQL para enviar instruções para o banco de dados
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection; //ligando o comando a conexão que configurada acima
+                cmd.CommandText = "puServico";
+                cmd.CommandType = CommandType.StoredProcedure; //definindo que o comando é um procedimento
+
+                //vincular os campos do formulários aos parâmetros do procedimento
+                cmd.Parameters.AddWithValue("nome", txtNome.Text);
+                cmd.Parameters.AddWithValue("descricao", txtDescricao.Text);
+                cmd.Parameters.AddWithValue("duracao", txtDuracao.Text);
+                cmd.Parameters.AddWithValue("valor", txtValor.Text);
+                cmd.Parameters.AddWithValue("idServico", txtID.Text);
+
+                connection.Open();
+                cmd.ExecuteNonQuery(); //executar para comandos que não possuem retorno de dados (INSERT, UPDATE e DELETE)
+                connection.Close();
+
+                MessageBox.Show("Serviço atualizado com sucesso!");
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            buscar();
+        }
+
+        private void buscar()
+        {
+            //criando o objeto de conexão com banco de dados
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "Server=turmassenacsantos.mssql.somee.com;Database=turmassenacsantos;User Id=senacclovis_SQLLogin_1;Password=n1642mlxmm;";
+
+            //criando o objeto de comando de SQL para enviar instruções para o banco de dados
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection; //ligando o comando a conexão que configurada acima
+            cmd.CommandText = "psServico";
+            cmd.CommandType = CommandType.StoredProcedure; //definindo que o comando é um procedimento
+
+            cmd.Parameters.AddWithValue("idServico", txtID.Text);
+
+            connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader(); //executando o comando de busca no SQL e armazenando o resultado da busca em um leitor (matriz)
+
+            if (reader.HasRows) //se o leitor tem linhas de dados
+            {
+                reader.Read(); //ler a próxima linha de dados
+
+                //tipos de GET em C#
+
+                //GetString = textos (VARCHAR)
+                //GetDecimal = valores decimais (DECIMAL)
+                //GetInt32 = números inteiros (INT)
+                //GetInt64 = números inteiros (BIGINT)
+                //GetDateTime = data (DATE, TIME ou DATETIME)
+
+                txtNome.Text = reader.GetString(1);
+                txtDescricao.Text = reader.GetString(2);
+                txtDuracao.Text = reader.GetInt32(3).ToString();
+                txtValor.Text = reader.GetDecimal(4).ToString(".");
+
+            }
+            else
+            {
+                MessageBox.Show("Serviço não encontrado!");
+            }
+
+            connection.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (txtID.TextLength > 0)
+            {
+                buscar();
+            }
+        }
+
+        
+
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+
+        {
+            
+
+            DialogResult result = MessageBox.Show("Deseja realmente excluir?", "Aviso", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                //criando o objeto de conexão com banco de dados
+                SqlConnection connection = new SqlConnection();
+                connection.ConnectionString = "Server=turmassenacsantos.mssql.somee.com;Database=turmassenacsantos;User Id=senacclovis_SQLLogin_1;Password=n1642mlxmm;";
+
+                //criando o objeto de comando de SQL para enviar instruções para o banco de dados
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection; //ligando o comando a conexão que configurada acima
+                cmd.CommandText = "pdServico";
+                cmd.CommandType = CommandType.StoredProcedure; //definindo que o comando é um procedimento
+
+                //vincular os campos do formulários aos parâmetros do procedimento
+                cmd.Parameters.AddWithValue("idServico", txtID.Text);
+
+                connection.Open();
+                cmd.ExecuteNonQuery(); //executar para comandos que não possuem retorno de dados (INSERT, UPDATE e DELETE)
+                connection.Close();
+
+                MessageBox.Show("Serviço excluído com sucesso!");
+
+                
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
